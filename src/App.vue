f5a8daaa0777b552c79226813f0e866d890b8519
fc836731493 <AntonyQQ998>
2019-06-04 20:17:26 +0800

    第一次上次

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+
+    <img src="./assets/logo.png">
+    <img src="../images/bt-zhaoxian2.jpg" width="366" height="20">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
